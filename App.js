b57bdadfbb2a6fc36076d9a531eae72332ca01c4
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-04-16 11:36:18 +0530

    section list

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { View, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './components/WelcomeScreen';
+import SectionList from './components/SectionList';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LittleLemonHeader />
-      <WelcomeScreen />
+      {/* <WelcomeScreen /> */}
+      <SectionList />
       <LittleLemonFooter />
     </View>
   );
